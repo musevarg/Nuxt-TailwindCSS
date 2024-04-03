84e6fe26d1bca1089a3d8c45efab711e708d2f09
musevarg <gravesum5@gmail.com>
2024-04-03 13:17:22 +0200

    content

--- app.vue
+++ app.vue
@@ -1,5 +1,62 @@
 <template>
-  <div>
-    <NuxtWelcome />
+<div class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+  <div class="absolute inset-0 -z-10 overflow-hidden">
+    <svg class="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
+      <defs>
+        <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+          <path d="M100 200V.5M.5 .5H200" fill="none" />
+        </pattern>
+      </defs>
+      <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
+        <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
+      </svg>
+      <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
+    </svg>
   </div>
+  <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+    <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+      <div class="lg:pr-4">
+        <div class="lg:max-w-lg">
+          <p class="text-base font-semibold leading-7 text-indigo-600">Quote</p>
+          <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Angus Young</h1>
+          <p class="mt-6 text-xl leading-8 text-gray-700">I'm sick to death of people saying we've made 11 albums that sounds exactly the same, Infact, we've made 12 albums that sound exactly the same.</p>
+        </div>
+      </div>
+    </div>
+    <div class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+      <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="https://societyofrock.com/wp-content/uploads/2016/08/acdcc.jpg" alt="">
+    </div>
+    <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+      <div class="lg:pr-4">
+        <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+          <p>AC/DC, Australian heavy metal band whose theatrical high-energy shows placed them among the most popular stadium performers of the 1980s. The principal members were Angus Young, Malcolm Young, Bon Scott, Brian Johnson, Phil Rudd, and Cliff Williams.</p>
+          <ul role="list" class="mt-8 space-y-8 text-gray-600">
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M6.8618,11.2764 L10.8618,3.27639 C10.9465,3.107 11.1196,3 11.309,3 L17.0397,3 C17.4442,3 17.6813,3.45534 17.4493,3.78673 L15.0507,7.21327 C14.8187,7.54466 15.0558,8 15.4603,8 L16.9129,8 C17.3401,8 17.5705,8.50106 17.2925,8.8254 L7.96557,19.7068 C7.59763,20.1361 6.91173,19.7207 7.1217,19.1957 L9.72572,12.6857 C9.85709,12.3573 9.61522,12 9.26148,12 L7.30902,12 C6.93733,12 6.69558,11.6088 6.8618,11.2764 Z" clip-rule="evenodd" />
+              </svg>
+              <span><strong class="font-semibold text-gray-900">The Young brothers</strong> formed AC/DC in Sydney, Australia, in 1973 with Angus (famous for his schoolboy short-trousers outfit) on lead guitar and Malcolm on rhythm guitar.</span>
+            </li>
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M6.8618,11.2764 L10.8618,3.27639 C10.9465,3.107 11.1196,3 11.309,3 L17.0397,3 C17.4442,3 17.6813,3.45534 17.4493,3.78673 L15.0507,7.21327 C14.8187,7.54466 15.0558,8 15.4603,8 L16.9129,8 C17.3401,8 17.5705,8.50106 17.2925,8.8254 L7.96557,19.7068 C7.59763,20.1361 6.91173,19.7207 7.1217,19.1957 L9.72572,12.6857 C9.85709,12.3573 9.61522,12 9.26148,12 L7.30902,12 C6.93733,12 6.69558,11.6088 6.8618,11.2764 Z" clip-rule="evenodd" />
+              </svg>
+              <span><strong class="font-semibold text-gray-900">AC/DC</strong>’s rise was hampered by Scott’s alcohol-related death in February 1980, but replacement Johnson’s falsetto fit in well with the group’s tight, clean metal punch and their raucous bad-boy image.</span>
+            </li>
+            <li class="flex gap-x-3">
+              <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path d="M6.8618,11.2764 L10.8618,3.27639 C10.9465,3.107 11.1196,3 11.309,3 L17.0397,3 C17.4442,3 17.6813,3.45534 17.4493,3.78673 L15.0507,7.21327 C14.8187,7.54466 15.0558,8 15.4603,8 L16.9129,8 C17.3401,8 17.5705,8.50106 17.2925,8.8254 L7.96557,19.7068 C7.59763,20.1361 6.91173,19.7207 7.1217,19.1957 L9.72572,12.6857 C9.85709,12.3573 9.61522,12 9.26148,12 L7.30902,12 C6.93733,12 6.69558,11.6088 6.8618,11.2764 Z" />
+              </svg>
+              <span><strong class="font-semibold text-gray-900">By the 1990s</strong> AC/DC found itself comfortably ensconced among the elder statesmen of heavy metal. The Razor’s Edge (1990) featured the hit singles “Thunderstruck” and “Moneytalks”.</span>
+            </li>
+          </ul>
+          <p class="mt-8">After more than 30 years of producing some of the roughest and loudest head-banging anthems in heavy metal history, AC/DC scored its first Billboard number one album with Black Ice (2008). The band reached another milestone in 2010 when it collected its first Grammy Award (in the category of best hard rock performance) for the single “War Machine.”</p>
+
+          <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">Heavy Metal</h2>
+          <p class="mt-6">Heavy metal, genre of rock music that includes a group of related styles that are intense, virtuosic, and powerful. Driven by the aggressive sounds of the distorted electric guitar, heavy metal is arguably the most commercially successful genre of rock music.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
